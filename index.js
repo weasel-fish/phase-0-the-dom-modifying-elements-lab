cbc09035a2d6df2708a6251710e2a12dc8e7dde6
Kyle Ermentrout <kyle.ermentrout@gmail.com>
2021-04-21 12:30:43 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.getElementById('main').remove();
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "Kyle Ermentrout is the champion"
+body.appendChild(newHeader);
